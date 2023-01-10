be1930e9bb3dd7725a3ff5aedf644f984936da41
Mab <mabchhan@gmail.com>
2023-01-09 18:24:44 -0800

    added function to view all department

--- server.js
+++ server.js
@@ -5,21 +5,88 @@ const console_table = require("console.table");
 // for load environment variables
 require("dotenv").config();
 
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    // MySQL username,
-    user: process.env.user,
-    //  MySQL password
-    password: process.env.password,
-    database: "employees_db",
-  },
-  console.log("Connected to the employees_db database.")
-);
+const db = mysql.createConnection({
+  host: "localhost",
+  // MySQL username,
+  user: process.env.user,
+  //  MySQL password
+  password: process.env.password,
+  database: "employees_db",
+});
 
-// db.connect((err) => {
-//   if (err) throw err;
-//   else {
-//     console.log("Connected to the employees_db database.");
-//   }
-// });
+db.connect((err) => {
+  if (err) throw err;
+  else {
+    console.log("Connected to the employees_db database.");
+    start();
+  }
+});
+
+// function to start app
+function start() {
+  inquirer
+    .prompt({
+      name: "questions",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update employee role",
+        "Exit",
+      ],
+    })
+    .then((answer) => {
+      switch (answer.questions) {
+        case "View all departments":
+          console.log("View all departments");
+          viewDepartment();
+          start();
+          break;
+
+        case "View all roles":
+          console.log("View all roles");
+
+          break;
+
+        case "View all employees":
+          console.log("View all employees");
+          break;
+
+        case "Add a department":
+          console.log("Add a department");
+          break;
+
+        case "Add a role":
+          console.log("Add a role");
+          break;
+
+        case "Add an employee":
+          console.log("Add a employee");
+          break;
+
+        case "Update employee role":
+          console.log("Update employee role");
+          break;
+
+        case "Exit":
+          connection.end();
+          break;
+      }
+    });
+}
+
+const viewDepartment = () => {
+  const sql = `SELECT * FROM department`;
+  db.query(sql, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      console.table(data);
+    }
+  });
+};
